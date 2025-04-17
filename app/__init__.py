12ae65e834825bea259c09dd253c8a0483aa4639
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-04-16 18:42:47 -0700

    Add book class, books enpoind

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
-from .routes.hello_world_routes import hello_world_bp
+#from .routes.hello_world_routes import hello_world_bp    # uses for hello_word_routes
+from .routes.book_routes import books_bp
+
 
 def create_app():
     app = Flask(__name__)
 
     # Register Blueprints here
-    app.register_blueprint(hello_world_bp)
+    #app.register_blueprint(hello_world_bp)     # uses for hello_word_routes
+    app.register_blueprint(books_bp)
 
     return app
