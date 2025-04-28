f376e2a15aa02e44aae219ce1d4fce578b29d6b1
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-04-27 17:46:19 -0700

    Implementat Wave_4

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
-#from .routes.hello_world_routes import hello_world_bp    # uses for hello_word_routes
+from .db import db, migrate
+from .models import book
 from .routes.book_routes import books_bp
-
 
 def create_app():
     app = Flask(__name__)
+# Database Migration: Connect the model seamlessly with our SQL database
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     # Register Blueprints here
-    #app.register_blueprint(hello_world_bp)     # uses for hello_word_routes
     app.register_blueprint(books_bp)
 
     return app
---
+++ app/models/base.py
@@ -0,0 +1,5 @@
+# Database Migration: Connect the model seamlessly with our SQL database
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
